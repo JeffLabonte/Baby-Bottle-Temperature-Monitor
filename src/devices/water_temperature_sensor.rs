a54307ba1c57cc017bfa82e6ea002d268d4d376d
Jeff Labonte <grimsleepless@protonmail.com>
2023-03-26 18:13:28 +0000

    Add GPIO dependencies and implement to input_device

--- src/devices/water_temperature_sensor.rs
+++ src/devices/water_temperature_sensor.rs
@@ -1,3 +1,13 @@
-pub struct WaterTemperatureSensor {}
+use gpio::{sysfs::SysFsGpioInput, GpioIn};
 
-impl WaterTemperatureSensor {}
+pub struct WaterTemperatureSensor {
+    input_device: SysFsGpioInput,
+}
+
+impl WaterTemperatureSensor {
+    pub fn new(pin: u16) -> Self {
+        Self {
+            input_device: SysFsGpioInput::open(pin).unwrap(),
+        }
+    }
+}
