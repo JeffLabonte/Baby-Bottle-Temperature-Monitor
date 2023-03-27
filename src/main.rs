fdbec02b899fee33acdfc494184fbddbbc279877
Jeff Labonte <grimsleepless@protonmail.com>
2023-03-27 06:30:43 +0000

    Use log instead of print

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 mod devices;
 
+use log::info;
+
 use crate::devices::water_temperature_sensor::WaterTemperatureSensor;
 
 fn main() {
     let mut water_temperature_sensor = WaterTemperatureSensor::new();
     loop {
-        println!("Temperature: {}", water_temperature_sensor.read());
+        info!("Temperature: {}", water_temperature_sensor.read());
         std::thread::sleep(std::time::Duration::from_secs(1));
     }
 }
