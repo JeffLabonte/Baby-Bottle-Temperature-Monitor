e6e45ea222ece53c6eab0399cf7aac740a8b6496
Jeff Labonte <grimsleepless@protonmail.com>
2023-03-27 06:28:03 +0000

    Add test code to read temperature

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,9 @@ mod devices;
 use crate::devices::water_temperature_sensor::WaterTemperatureSensor;
 
 fn main() {
-    println!("Hello, world!");
+    let mut water_temperature_sensor = WaterTemperatureSensor::new();
+    loop {
+        println!("Temperature: {}", water_temperature_sensor.read());
+        std::thread::sleep(std::time::Duration::from_secs(1));
+    }
 }
