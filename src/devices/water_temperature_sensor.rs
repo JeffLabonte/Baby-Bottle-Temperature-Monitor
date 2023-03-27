787e0fd71c5de73ea6e88b3a887256d03032bdb1
Jeff Labonte <grimsleepless@protonmail.com>
2023-03-27 06:25:32 +0000

    Create code to read temperature

--- src/devices/water_temperature_sensor.rs
+++ src/devices/water_temperature_sensor.rs
@@ -1,7 +1,41 @@
-pub struct WaterTemperatureSensor {}
+use std::fs;
+
+const BASE_DIR_TEMPERATURE_SENSOR: &str = "/sys/bus/w1/devices/";
+
+pub struct WaterTemperatureSensor {
+    temperature_filepath: String,
+}
 
 impl WaterTemperatureSensor {
+    pub fn new() -> Self {
+        WaterTemperatureSensor {
+            temperature_filepath: WaterTemperatureSensor::get_temperature_filepath().unwrap(),
+        }
+    }
+
     pub fn read(&mut self) -> f32 {
-        0.0
+        fs::read_to_string(&self.temperature_filepath)
+            .unwrap()
+            .trim()
+            .parse::<f32>()
+            .unwrap()
+            / 1000.0
+    }
+
+    fn get_temperature_filepath() -> Result<String, String> {
+        let directories = fs::read_dir(BASE_DIR_TEMPERATURE_SENSOR).unwrap();
+        let mut base_directory_copy = String::from(BASE_DIR_TEMPERATURE_SENSOR);
+
+        for directory in directories {
+            let directory = directory.unwrap();
+            let directory_name = directory.file_name().into_string().unwrap();
+
+            if directory_name.starts_with("28-") {
+                base_directory_copy.push_str(&directory_name);
+                base_directory_copy.push_str("temperature");
+                return Ok(base_directory_copy);
+            }
+        }
+        Err(String::from("Unable to find temperature sensor"))
     }
 }
