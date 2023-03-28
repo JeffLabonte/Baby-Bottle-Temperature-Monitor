ee52538cb5b67993d145639aefb614292d101877
Jeff Labonte <grimsleepless@protonmail.com>
2023-03-28 06:16:35 +0000

    Add code to notify phone

--- src/devices/water_temperature_sensor.rs
+++ src/devices/water_temperature_sensor.rs
@@ -6,23 +6,40 @@ const BASE_DIR_TEMPERATURE_SENSOR: &str = "/sys/bus/w1/devices/";
 
 pub struct WaterTemperatureSensor {
     temperature_filepath: String,
+    current_temperature: f32,
+    temperature_threshold: f32,
+    temperature_has_changed: bool,
 }
 
 impl WaterTemperatureSensor {
     pub fn new() -> Self {
         WaterTemperatureSensor {
             temperature_filepath: WaterTemperatureSensor::get_temperature_filepath().unwrap(),
+            current_temperature: 0.0,
+            temperature_threshold: 30.0,
+            temperature_has_changed: false,
         }
     }
 
     pub fn read(&mut self) -> f32 {
         info!("Reading temperature from {}\n", self.temperature_filepath);
-        fs::read_to_string(&self.temperature_filepath)
+        self.current_temperature = fs::read_to_string(&self.temperature_filepath)
             .unwrap()
             .trim()
             .parse::<f32>()
             .unwrap()
-            / 1000.0
+            / 1000.0;
+
+        self.set_temperature_has_changed();
+        self.current_temperature
+    }
+
+    pub fn get_has_temperature_changed(&self) -> bool {
+        self.temperature_has_changed
+    }
+
+    fn set_temperature_has_changed(&mut self) {
+        self.temperature_has_changed = self.current_temperature > self.temperature_threshold;
     }
 
     fn get_temperature_filepath() -> Result<String, String> {
--- src/main.rs
+++ src/main.rs
@@ -4,36 +4,50 @@ use std::env;
 
 use crate::devices::water_temperature_sensor::WaterTemperatureSensor;
 
+async fn publish_message_to_sms(
+    client: &aws_sdk_sns::Client,
+    temperature: f32,
+    phone_number: &String,
+    topic_arn: &String,
+) -> () {
+    client
+        .subscribe()
+        .topic_arn(topic_arn)
+        .protocol("sms")
+        .endpoint(phone_number)
+        .send()
+        .await
+        .unwrap();
+
+    client
+        .publish()
+        .topic_arn(topic_arn)
+        .message(&format!("The water temperature is {} Celcius", temperature))
+        .send()
+        .await
+        .unwrap();
+}
+
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().ok();
 
     let config = aws_config::load_from_env().await;
     let client = aws_sdk_sns::Client::new(&config);
-
     let phone_number = env::var("PHONE_NUMBER").expect("PHONE_NUMBER must be set");
     let topic_arn = env::var("TOPIC_ARN").expect("TOPIC_ARN must be set");
+    let mut phone_notified = false;
 
     let mut water_temperature_sensor = WaterTemperatureSensor::new();
     loop {
         let temperature = water_temperature_sensor.read();
         println!("Temperature: {}", temperature);
         std::thread::sleep(std::time::Duration::from_secs(1));
-        client
-            .subscribe()
-            .topic_arn(&topic_arn)
-            .protocol("sms")
-            .endpoint(&phone_number)
-            .send()
-            .await
-            .unwrap();
 
-        client
-            .publish()
-            .topic_arn(&topic_arn)
-            .message(&format!("The water temperature is {}", temperature))
-            .send()
-            .await
-            .unwrap();
+        if water_temperature_sensor.get_has_temperature_changed() && !phone_notified {
+            publish_message_to_sms(&client, temperature, &phone_number, &topic_arn).await;
+        } else if !water_temperature_sensor.get_has_temperature_changed() && phone_notified {
+            phone_notified = false;
+        }
     }
 }
