9a2767fb44a32716a38de0018f8f67b96d43d690
Jeff Labonte <grimsleepless@protonmail.com>
2023-03-28 03:06:43 +0000

    Add dotenv and add AWS code to send notification

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
 mod devices;
 
+use std::env;
+
 use crate::devices::water_temperature_sensor::WaterTemperatureSensor;
 
 #[tokio::main]
 async fn main() {
+    dotenv::dotenv().ok();
+
     let config = aws_config::load_from_env().await;
     let client = aws_sdk_sns::Client::new(&config);
 
+    let phone_number = env::var("PHONE_NUMBER").expect("PHONE_NUMBER must be set");
+    let topic_arn = env::var("TOPIC_ARN").expect("TOPIC_ARN must be set");
+
     let mut water_temperature_sensor = WaterTemperatureSensor::new();
     loop {
-        println!("Temperature: {}", water_temperature_sensor.read());
+        let temperature = water_temperature_sensor.read();
+        println!("Temperature: {}", temperature);
         std::thread::sleep(std::time::Duration::from_secs(1));
+        client
+            .subscribe()
+            .topic_arn(&topic_arn)
+            .protocol("sms")
+            .endpoint(&phone_number)
+            .send()
+            .await
+            .unwrap();
     }
 }
