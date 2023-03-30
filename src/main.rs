7b5face8491555a0275f27d924bc70606055043f
Jeff Labonte <grimsleepless@protonmail.com>
2023-03-30 14:49:15 +0000

    Move query code together

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,15 @@ use twilio::OutboundMessage;
 
 use crate::devices::water_temperature_sensor::WaterTemperatureSensor;
 
-async fn publish_message_to_sms(
-    client: &twilio::Client,
-    temperature: f32,
-    to_phone_number: &String,
-    from_phone_number: &String,
-) -> () {
-    println!(
-        "Sending message to {} from {}",
-        to_phone_number, from_phone_number
-    );
+async fn publish_message_to_sms(temperature: f32) -> () {
+    let twilio_account_id = env::var("TWILIO_ACCOUNT_ID").expect("TWILIO_ACCOUNT_ID must be set");
+    let twilio_auth_token = env::var("TWILIO_AUTH_TOKEN").expect("TWILIO_AUTH_TOKEN must be set");
+
+    let client = twilio::Client::new(twilio_account_id.as_str(), twilio_auth_token.as_str());
+
+    let to_phone_number = env::var("TO_PHONE_NUMBER").expect("PHONE_NUMBER must be set");
+    let from_phone_number = env::var("FROM_PHONE_NUMBER").expect("PHONE_NUMBER must be set");
+
     let response = client
         .send_message(OutboundMessage::new(
             from_phone_number.as_str(),
@@ -33,15 +32,7 @@ async fn publish_message_to_sms(
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().ok();
-    let twilio_account_id = env::var("TWILIO_ACCOUNT_ID").expect("TWILIO_ACCOUNT_ID must be set");
-    let twilio_auth_token = env::var("TWILIO_AUTH_TOKEN").expect("TWILIO_AUTH_TOKEN must be set");
-    println!("{}", twilio_account_id);
-    println!("{}", twilio_auth_token);
 
-    let client = twilio::Client::new(twilio_account_id.as_str(), twilio_auth_token.as_str());
-
-    let to_phone_number = env::var("TO_PHONE_NUMBER").expect("PHONE_NUMBER must be set");
-    let from_phone_number = env::var("FROM_PHONE_NUMBER").expect("PHONE_NUMBER must be set");
     let mut phone_notified = false;
 
     let mut water_temperature_sensor = WaterTemperatureSensor::new();
@@ -56,8 +47,7 @@ async fn main() {
             water_temperature_sensor.reset_temperature_back_to_normal()
         } else if water_temperature_sensor.is_temperature_back_to_normal() && !phone_notified {
             println!("Notifying user ...");
-            publish_message_to_sms(&client, temperature, &to_phone_number, &from_phone_number)
-                .await;
+            publish_message_to_sms(temperature).await;
             phone_notified = true;
         }
     }
