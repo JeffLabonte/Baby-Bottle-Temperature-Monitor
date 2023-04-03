5adb3a0482573910a002ea9d048a82935699785e
Jean-Francois Labonte <grimsleepless@protonmail.com>
2023-04-03 03:21:37 -0400

    Update code to handle multiple phone numbers

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,33 @@ use twilio::OutboundMessage;
 
 use crate::devices::water_temperature_sensor::WaterTemperatureSensor;
 
+fn get_phone_numbers() -> Vec<String> {
+    env::var("TO_PHONE_NUMBERS")
+        .expect("TO_PHONE_NUMBERS must be set")
+        .split(",")
+        .map(|x| x.trim().to_string())
+        .collect()
+}
+
 async fn publish_message_to_sms(temperature: f32) -> () {
     let twilio_account_id = env::var("TWILIO_ACCOUNT_ID").expect("TWILIO_ACCOUNT_ID must be set");
     let twilio_auth_token = env::var("TWILIO_AUTH_TOKEN").expect("TWILIO_AUTH_TOKEN must be set");
 
     let client = twilio::Client::new(twilio_account_id.as_str(), twilio_auth_token.as_str());
 
-    let to_phone_number = env::var("TO_PHONE_NUMBER").expect("PHONE_NUMBER must be set");
+    let to_phone_numbers = get_phone_numbers();
     let from_phone_number = env::var("FROM_PHONE_NUMBER").expect("PHONE_NUMBER must be set");
 
-    let response = client
-        .send_message(OutboundMessage::new(
-            from_phone_number.as_str(),
-            to_phone_number.as_str(),
-            format!("The temperature is {}", temperature).as_str(),
-        ))
-        .await;
-    println!("Response: {:?}", response);
+    for to_phone_number in to_phone_numbers {
+        let response = client
+            .send_message(OutboundMessage::new(
+                from_phone_number.as_str(),
+                to_phone_number.as_str(),
+                format!("The temperature is {}", temperature).as_str(),
+            ))
+            .await;
+        println!("Response: {:?}", response);
+    }
 }
 
 #[tokio::main]
