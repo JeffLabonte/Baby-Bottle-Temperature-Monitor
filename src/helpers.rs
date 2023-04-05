a74d0c074ebdbc7e7c6868591c93caca7d830705
Jean-Francois Labonte <grimsleepless@protonmail.com>
2023-04-05 01:27:32 -0400

    Update write_file to handle not existing file

--- src/helpers.rs
+++ src/helpers.rs
@@ -3,7 +3,10 @@ use std::fs::File;
 use std::io::prelude::*;
 
 pub fn write_to_file(file_path: String, message: String) {
-    let mut file: File = File::options().append(true).open(&file_path).unwrap();
+    let mut file: File = match File::options().append(true).open(&file_path) {
+        Ok(f) => f,
+        Err(_) => File::create(&file_path).unwrap(),
+    };
     let result = file.write_all(message.as_bytes());
     match result {
         Ok(_) => (),
