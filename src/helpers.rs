ad31a6f88190ec97bf888d4cebf879266cb325b5
Jean-Francois Labonte <grimsleepless@protonmail.com>
2023-04-05 00:50:32 -0400

    Add a helpers module

---
+++ src/helpers.rs
@@ -0,0 +1,9 @@
+use std::fs::File;
+
+pub fn write_to_file(file_path: String, message: String) {
+    let mut file = File::create(format!(""));
+}
+
+pub fn generate_file_name_with_now_time() -> String {
+    String::new()
+}
