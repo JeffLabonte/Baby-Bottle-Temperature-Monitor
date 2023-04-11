775fa1056cb87b78b61dfc73ddb3fa7e60f738ac
Jean-Francois Labonte <grimsleepless@protonmail.com>
2023-04-10 23:30:19 -0400

    Use static variable in the function

--- src/data_collection.rs
+++ src/data_collection.rs
@@ -81,8 +81,8 @@ static DATA_COLLECTION_ENABLED_KEY: &str = "DATABASE_COLLECTION_ENABLED";
 static DATA_COLLECTION_SECRET_KEY: &str = "DATABASE_COLLECTION_SECRET";
 
 pub async fn collect_data(water_temperature_sensor: &WaterTemperatureSensor) -> () {
-    let collection_enabled =
-        env::var("DATA_COLLECTION_ENABLED").expect("DATA_COLLECTION_ENABLED must be set");
+    let collection_enabled = env::var(DATA_COLLECTION_ENABLED_KEY.to_string())
+        .expect("DATA_COLLECTION_ENABLED must be set");
 
     if collection_enabled.trim().parse().unwrap() && water_temperature_sensor.should_collect_data()
     {
@@ -92,13 +92,16 @@ pub async fn collect_data(water_temperature_sensor: &WaterTemperatureSensor) -> 
             water_temperature_sensor.current_temperature,
         );
 
-        let url = env::var("DATA_COLLECTION_URL").expect("DATA_COLLECTION_URL must be set");
-        let data_collection_auth =
-            env::var("DATA_COLLECTION_SECRET").expect("DATA_COLLECTION_SECRET must be set");
-        let result_query = reqwest::Client::new()
+        let url =
+            env::var(DATA_COLLECTION_URL_KEY.to_string()).expect("DATA_COLLECTION_URL must be set");
+
+        let data_collection_auth = env::var(DATA_COLLECTION_SECRET_KEY.to_string())
+            .expect("DATA_COLLECTION_SECRET must be set");
+
+        let result_query = Client::new()
             .post(url)
             .header("Content-Type", "application/json")
-            .header("X-Require-Whisk-Auth", data_collection_auth)
+            .header("X-Require-Whisk-Auth", data_collection_auth.as_str())
             .json(&json_body)
             .send()
             .await;
